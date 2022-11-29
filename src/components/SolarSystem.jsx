bce527168fe50fbd403b66971fbce25c7720c5f8
Allan Medeiros Minato <allan.minato@gmail.com>
2022-11-28 23:03:39 -0300

    Resolução exercício 6

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <h3>
-          {' '}
           <Title headline="Planetas" />
         </h3>
+        {planets.map(({ name, image }) => (
+          <PlanetCard
+            key={ name }
+            planetName={ name }
+            planetImage={ image }
+          />
+        ))}
       </div>
     );
   }
 }
-
+// Para cada planeta da lista, você deverá renderizar um componente PlanetCard, passando 
+// o atributo name para a prop planetName e o atributo image para a prop planetImage.
 export default SolarSystem;
