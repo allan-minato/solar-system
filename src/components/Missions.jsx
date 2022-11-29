0bd08bc5642b3be6ecca0d4a1b380bea5443577b
Allan Medeiros Minato <allan.minato@gmail.com>
2022-11-29 01:35:36 -0300

    Resolução exercício 10

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {missions.map(({ name, year, country, destination }) => (
+          <MissionCard
+            key={ name }
+            name={ name }
+            year={ year }
+            country={ country }
+            destination={ destination }
+          />
+        ))}
       </div>
     );
   }
