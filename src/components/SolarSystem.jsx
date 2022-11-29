6073c01cd5163863900b23faa6644c5838288952
Allan Medeiros Minato <allan.minato@gmail.com>
2022-11-28 23:11:27 -0300

    Removido comentário onde apresentava erro no lint

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -21,6 +21,5 @@ class SolarSystem extends React.Component {
     );
   }
 }
-// Para cada planeta da lista, você deverá renderizar um componente PlanetCard, passando 
-// o atributo name para a prop planetName e o atributo image para a prop planetImage.
+
 export default SolarSystem;
